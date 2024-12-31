09295f7ef06d05af2b1b1bf88744109c5b615d70
Zanderskier <46981525+Zanderskier@users.noreply.github.com>
2024-12-31 10:03:06 -0800

    S3 bucket fixes and corrections

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { storage } from "./storage/resource";
 
 export const backend = defineBackend({
   auth,
   data,
-  storage,
 });
